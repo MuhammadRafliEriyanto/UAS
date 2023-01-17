4447fdb2435f31a045d108d25f979a79be6bf9fa
MuhammadRafliEriyantow <raflimhmmd6212gmail.com>
2023-01-17 12:11:45 +0700

    done

--- src/No2.java
+++ src/No2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class No2 {
+public class No2 {
+
+    public static void main(String[] args) {
+        System.out.println(ReverseString.Reverse("a b cd ef"));
+        System.out.println(ReverseString.Reverse("contoh kalimat"));
+    }
+    public static No2 ReverseString;
+
+    private static String Reverse(String dolphins){
+        char raf[]=dolphins.toCharArray();
+        String gogo="";
+        for(int i=raf.length-1;i>-0;i--){
+            gogo+=raf[i];
+        }
+return gogo.replaceAll("(?)[aiueo]","*");
+    }
 }
+
